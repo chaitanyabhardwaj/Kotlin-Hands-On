beaaa4082822dfa8379aa67660be7a32bdd34e6e
Chaitanya Bhardwaj <chaitanyabhardwaj89@gmail.com>
2023-05-22 01:06:33 +0530

    Kotlin, Hello World

---
+++ chaitanyabhardwaj/git/kotlin/HelloWorldKotlin.kt
@@ -0,0 +1,8 @@
+package chaitanyabhardwaj.git.kotlin;
+
+fun main() {
+	println("Hello, World!")
+}
+
+//I'm running this program in vs code using the vscode-runner plugin
+//which requires us to install the kotlin plugin separately
